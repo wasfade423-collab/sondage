1c0712fb20b4f50d2fc85908132719e57bd181d8
wasfade <wasfade423@gmail.com>
2026-02-05 04:12:26 +0100

    Avec Login Admin

---
+++ vue/adminLogin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <p>Veuillez vous authentifiez</p>
+        <input type="text" name="login" placeholder="Login" id=""><br>
+        <input type="password" name="" id="" placeholder="Mot de Pass"><br>
+        <input type="submit" value="Se connectez">
+    </form>
+</body>
+</html>
